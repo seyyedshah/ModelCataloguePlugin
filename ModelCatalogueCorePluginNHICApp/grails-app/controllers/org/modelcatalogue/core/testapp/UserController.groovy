7465a20e06a8dfd9562c5d740333fa7644e29155
soheilsaif <s.saifipour@gmail.com>
2014-09-16 21:35:32 +0100

    Adding customized spring security UserController

--- ModelCatalogueCorePluginNHICApp/grails-app/controllers/org/modelcatalogue/core/testapp/UserController.groovy
+++ ModelCatalogueCorePluginNHICApp/grails-app/controllers/org/modelcatalogue/core/testapp/UserController.groovy
@@ -1,4 +1,47 @@
 package org.modelcatalogue.core.testapp
 
+import grails.plugin.springsecurity.authentication.dao.NullSaltSource
+import grails.plugin.springsecurity.ui.RegistrationCode
+
 class UserController extends grails.plugin.springsecurity.ui.UserController {
+
+
+
+
+
+
+	/**
+	 * Create a user from the passed params
+	 */
+	def save = {
+		def user = lookupUserClass().newInstance(params)
+		if (params.password) {
+			String salt = saltSource instanceof NullSaltSource ? null : params.username
+			user.password = springSecurityUiService.encodePassword(params.password, salt)
+		}
+		if (!user.save(flush: true)) {
+			render view: 'create', model: [user: user, authorityList: sortedRoles()]
+			return
+		}
+
+		addRoles(user)
+		flash.message = "${message(code: 'default.created.message', args: [message(code: 'user.label', default: 'User'), user.id])}"
+
+		def registrationCode = new RegistrationCode(username: user.username)
+		registrationCode.save(flush: true)
+		String url = generateLink('resetPassword', [t: registrationCode.token])
+
+
+		def siteUrl = "$request.scheme://$request.serverName:$request.serverPort$request.contextPath"
+
+		sendMail {
+			to "${user.firstName} ${user.lastName}<${user.email}>"
+			// FIXME needs to be refactored into a messages class - i18n support
+			subject "Model Catalogue user account details"
+			html( g.render(template: "userCreatedEmail", model: [user: user, resetLink: url, mcUrl: siteUrl] ))
+		}
+
+		redirect action: 'edit', id: user.id
+	}
+
 }
