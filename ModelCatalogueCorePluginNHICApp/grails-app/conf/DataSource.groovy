7636bbbb65d5dbf5329c89af0c5d0059cdcbbad6
soheilsaif <s.saifipour@gmail.com>
2014-09-23 11:27:15 +0100

    Adding datasources for testing app locally

--- ModelCatalogueCorePluginNHICApp/grails-app/conf/DataSource.groovy
+++ ModelCatalogueCorePluginNHICApp/grails-app/conf/DataSource.groovy
@@ -1,31 +1,41 @@
 dataSource {
-    pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+	pooled = true
+	driverClassName = "org.h2.Driver"
+	username = "sa"
+	password = ""
 }
 hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = false
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
+	cache.use_second_level_cache = true
+	cache.use_query_cache = false
+	cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
 //    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
 }
 
 // environment specific settings
 environments {
-    development {
+	development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
-    }
-    test {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-        }
-    }
-    production {
+//
+//		dataSource {
+//			dialect = 'org.hibernate.dialect.MySQL5InnoDBDialect'
+//			pooled = true
+//			dbCreate = "update"
+//			url = "jdbc:mysql://localhost:13306/MCCOSD"
+//			driverClassName = "com.mysql.jdbc.Driver"
+//			username = "root"
+//		}
+
+	}
+	test {
+		dataSource {
+			dbCreate = "update"
+			url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+		}
+	}
+	production {
 //        dataSource {
 //            dialect = 'org.hibernate.dialect.MySQL5InnoDBDialect'
 //			pooled = true
@@ -35,11 +45,10 @@ environments {
 //			username = "root"
 //        }
 
-
 //        dataSource {
 //            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
 //            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
 //        }
 
-    }
+	}
 }
