b1570fa0c5be83d71bd53cba8b9481942952e47c
soheil.saifipour <soheil.saifipour@ndm.ox.ac.uk>
2015-07-16 11:13:57 +0100

    Updating name property in User object from username

--- ModelCatalogueCorePluginTestApp/grails-app/controllers/org/modelcatalogue/core/security/UserAdminController.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/controllers/org/modelcatalogue/core/security/UserAdminController.groovy
@@ -6,6 +6,14 @@ class UserAdminController extends grails.plugin.springsecurity.ui.UserController
         super.create()
     }
     def save(){
+		//update name(which is inherited from CatalogueElement) in User Class and fill it with username
+		params['name'] = params?.username;
         super.save()
     }
+
+	def update() {
+		//update name(which is inherited from CatalogueElement) in User Class and fill it with username
+		params['name'] = params?.username;
+		super.update()
+	}
 }
